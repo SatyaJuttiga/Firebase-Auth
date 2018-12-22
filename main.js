72dde1f0da734b9e41857888fce96f6ece1dbcd0
SATYA JUTTIGA <satyatanmayee@gmail.com>
2018-12-22 13:30:56 +0530

    js

---
+++ main.js
@@ -0,0 +1,19 @@
+
+var mainapp={};
+(function(){
+    var firebase=app_firebase;
+    var uid= null;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          uid=user.uid;
+        }else{
+            uid= null;
+            window.location.replace("login.html");
+        }
+      });
+      function logout(){
+          firebase.auth().signOut();
+      }
+      mainapp.logout=logout;
+})()
